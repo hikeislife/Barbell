12b9b2f4d48f9a6b4c0639b8f89fcfe126dbb8e2
IgorMilatovic <igby.veko@gmail.com>
2019-10-17 18:43:21 +0200

    style

--- web/themes/custom/barbell/js/main.js
+++ web/themes/custom/barbell/js/main.js
@@ -1,7 +1,116 @@
 Drupal.behaviors.exampleModule = {
-  attach: function (context, settings) {
-    //$('.example', context).click(function () {
-      console.log('test')
-    //});
+  attach: function(context, settings) {
+    let navigacija = (function() {
+      var langSwitch = document.querySelector("#block-languageswitcher");
+      var hamburgerHolder = document.createElement("div");
+      var hamburgerImg = document.createElement("img");
+      hamburgerImg.src = "/barbell/web/themes/custom/barbell/img/hamburger.png";
+      hamburgerHolder.classList.add("hamburgerHolder");
+      hamburgerHolder.appendChild(hamburgerImg);
+      langSwitch.appendChild(hamburgerHolder);
+
+      var nav = document.querySelector("header div nav");
+      var xHolder = document.createElement("div");
+      var xPara = document.createElement("p");
+      xPara.innerHTML = "X";
+      xHolder.appendChild(xPara);
+      nav.appendChild(xHolder);
+      xHolder.classList.add("xHolder");
+      xPara.classList.add("xPara");
+
+      hamburgerHolder.addEventListener("click", function() {
+        nav.style.top = "0";
+        xHolder.style.display = "block";
+
+        setTimeout(() => {
+          xPara.style.fontSize = "43px";
+          xPara.style.opacity = "1";
+        }, 300);
+
+        setTimeout(() => {
+          xHolder.style.border = "1px solid whitesmoke";
+        }, 900);
+
+        function closeNav() {
+          nav.style.top = "-60vh";
+          xHolder.style.display = "none";
+
+          // setTimeout(() => {
+          xPara.style.fontSize = "1px";
+          xPara.style.opacity = "0";
+          // }, 300);
+
+          // setTimeout(() => {
+          xHolder.style.border = "none";
+          // }, 900);
+        }
+
+        var navLinks = document.querySelectorAll(
+          "#block-barbell-main-menu ul li"
+        );
+
+        navLinks.forEach(link =>
+          link.addEventListener("click", function() {
+            if (nav.style.top == "0") {
+              closeNav();
+            }
+          })
+        );
+
+        xHolder.addEventListener("click", closeNav);
+      });
+
+      // TEMPORARY
+      // xPara.style.opacity = "0";
+      // xHolder.style.opacity = "0";
+      // TEMPORARY
+    })();
+
+    let raspored = (function() {
+      let td = document.querySelectorAll("td div");
+      td = [...td];
+      let text = "";
+      let icon = "";
+
+      td.forEach(x => {
+        if (x.classList[0] === "raspored-ft") {
+          text = "FUNKCIONALNI TRENING";
+          icon = "/barbell/web/themes/custom/barbell/img/fe.webp";
+        } else if (x.classList[0] === "raspored-se") {
+          text = "SNAGA I ESTETIKA";
+          icon = "/barbell/web/themes/custom/barbell/img/se.webp";
+        } else if (x.classList[0] === "raspored-as") {
+          text = "ASICS ŠKOLA TRČANJA";
+          icon = "/barbell/web/themes/custom/barbell/img/asics.webp";
+        }
+
+        (function fixContent() {
+          removeInitialContent();
+          createNewContent();
+        })();
+
+        function removeInitialContent() {
+          x.innerHTML = "";
+        }
+
+        function createNewContent() {
+          const imgContainer = document.createElement("div");
+          const img = document.createElement("img");
+          const titleContainer = document.createElement("div");
+          const title = document.createElement("p");
+
+          x.appendChild(imgContainer);
+          x.appendChild(titleContainer);
+          titleContainer.appendChild(title);
+          imgContainer.appendChild(img);
+
+          title.className = "rasporedText";
+          imgContainer.className = "rasporedImgWrapper";
+
+          title.innerHTML = text;
+          img.src = icon;
+        }
+      });
+    })();
   }
 };
