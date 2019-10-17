0aecb317ee152341d5de1997f67ba0e0fc65e52a
hikeislife <hikeislife@users.noreply.github.com>
2019-10-17 17:07:45 +0200

    treninzi respo

--- web/themes/custom/barbell/js/main.js
+++ web/themes/custom/barbell/js/main.js
@@ -1,7 +1,5 @@
 Drupal.behaviors.exampleModule = {
   attach: function (context, settings) {
-    //$('.example', context).click(function () {
-      console.log('test')
-    //});
+    console.log('test')
   }
 };
